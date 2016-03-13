d1b605fd6b7fbbb69469c6117ffb6268d2f07db0
auras76 <anghel0876@gmail.com>
2016-03-13 22:46:22 +0100

    MM : 32.1.A.1.163

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#10 SMP PREEMPT Fri Mar 11 00:12:46 CET 2016"
+#define LINUX_COMPILE_BY "auras"
+#define LINUX_COMPILE_HOST "auras-System-Product-Name"
+#define LINUX_COMPILER "gcc version 4.9.4 20160201 (optimized) (UBERTC-4.9.4) "
